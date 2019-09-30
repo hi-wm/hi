34781c2b27a27876f360a2c864fd8ae58b3a8aac
William Harrison <wm.harrison@gmail.com>
2019-09-30 17:28:45 -0500

    updated js

--- la-croix.js
+++ la-croix.js
@@ -5,7 +5,8 @@ let minus_weekends = ((day_diff / 7).toFixed(0))*2;
 let work_days = day_diff - minus_weekends;
 let la_croix_consumed = work_days * 2;
 la_croix.innerHTML = la_croix_consumed;
+let la_croix_cans = document.getElementById("la-croix-cans");
 
 for(let i = 0; i < la_croix_consumed; i++) {
-    document.getElementById("la-croix-cans").append(`<span id={i} class="la-croix-can"></span>`)
+    la_croix_cans.append(`<span id={i} class="la-croix-can"></span>`)
 }
